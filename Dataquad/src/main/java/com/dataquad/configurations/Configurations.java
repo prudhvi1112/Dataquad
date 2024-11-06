47515eaf1512501d11fea5eb41bb2e3fc8f511eb
issach315 <issachemani227@gmail.com>
2024-11-06 17:07:27 +0530

    new account

--- Dataquad/src/main/java/com/dataquad/configurations/Configurations.java
+++ Dataquad/src/main/java/com/dataquad/configurations/Configurations.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Configurations 
 {
+	// new branchgit
 	   @Bean
 	    public ModelMapper modelMapper() {
 	        return new ModelMapper();
